04988d66466ce052ac2f8d17607adf9ce91717bb
unknown <jamal.hansen@gmail.com>
2008-12-07 15:11:59 -0600

    adding test cases for true or false questions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,15 @@ end
 #$:.unshift(File.dirname(__FILE__) + '/../lib')
 $:.unshift(File.dirname(__FILE__) + '/../lib/gipper')
 require 'parser'
+
+def is_true string
+  string == "{T}" || string == "{True}"
+end
+
+def true_false_question title, question, answer
+  if title
+    "::#{title}::#{question}#{answer}"
+  else
+    "#{question}#{answer}"
+  end
+end

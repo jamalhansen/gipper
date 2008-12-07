d50a8698a57fd3e3c7e7999d51a1b6e1a2c0e224
unknown <jamal.hansen@gmail.com>
2008-12-06 23:28:23 -0600

    Getting this project off the ground with the first feature and spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,6 @@ rescue LoadError
   require 'spec'
 end
 
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-require 'gipper'
+#$:.unshift(File.dirname(__FILE__) + '/../lib')
+$:.unshift(File.dirname(__FILE__) + '/../lib/gipper')
+require 'parser'

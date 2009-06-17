09b3f824b62a840e7d0a7b98b5d3e6f30aecbf6d
Jamal Hansen <jamal.hansen@gmail.com>
2009-06-16 20:56:09 -0500

    moved matching feature to Test::Unit

--- features/steps/parse_matching_steps.rb
+++ features/steps/parse_matching_steps.rb
@@ -2,7 +2,7 @@
 Given /^a GIFT file of matching questions$/ do
   file = File.open(File.join(File.dirname(__FILE__), *%w[.. fixtures matching.gift]))
   @data = file.read
-  @data.length.should be > 0
+  assert @data.length > 0
 end
 
 Then /^contains the correct matching questions$/ do
@@ -14,19 +14,17 @@ Then /^contains the correct matching questions$/ do
   #=\(^_^)/ -> yay!
   #=:( -> sad
   #}
-  @questions.length.should be(1)
+  assert_equal 1, @questions.length
   
-  @questions[0].style.should eql(:matching)
-  @questions[0].text.should eql("Match the emoticon to the correct emotion:")
-  @questions[0].title.should eql("Intro to matching questions")
-  @questions[0].answer.length.should eql(4)
-  @questions[0].answer[0].text.should eql(":)")
-  @questions[0].answer[0].correct.should eql("smiling")
-  @questions[0].answer[1].text.should eql(":D")
-  @questions[0].answer[1].correct.should eql("crazy happy")
-  @questions[0].answer[2].text.should eql('\(^_^)/')
-  @questions[0].answer[2].correct.should eql("yay!")
-  @questions[0].answer[3].text.should eql(":(")
-  @questions[0].answer[3].correct.should eql("sad")
+  question = @questions[0]
+  assert_equal 4, question.answer.length
+  assert_question question, :style => :matching,
+                            :text => "Match the emoticon to the correct emotion:",
+                            :title => "Intro to matching questions"
+
+  assert_answer question.answer[0], :correct => "smiling", :text => ":)"
+  assert_answer question.answer[1], :correct => "crazy happy", :text => ":D"
+  assert_answer question.answer[2], :correct => "yay!", :text => '\\(^_^)/'
+  assert_answer question.answer[3], :correct => "sad", :text => ":("
 end
 
